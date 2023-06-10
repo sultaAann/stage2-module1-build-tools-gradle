c98ff47579e88cd68948344b413f6222c7deddf9
sultaAann <sultandjavgashev@gmail.com>
2023-06-10 14:46:32 +0500

    ok

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,8 @@
 package com.epam.utils;
 
+import org.apache.commons.lang3.math.NumberUtils;
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-         //here magic will happen
+        return NumberUtils.isCreatable(str) && Double.parseDouble(str) > 0;
     }
 }
